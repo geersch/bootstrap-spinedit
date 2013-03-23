a98aa3c6170bf3e7725d764a667e857828599013
Christophe Geers <geersch@gmail.com>
2013-03-23 10:24:20 +0100

    Removed $.browser(...) call to fix jQuery 1.9.x compatiblity

--- js/bootstrap-spinedit.js
+++ js/bootstrap-spinedit.js
@@ -31,13 +31,7 @@
 $(function () {
     $.extend($.fn.disableTextSelect = function () {
         return this.each(function () {
-            if ($.browser.mozilla) {//Firefox
-                $(this).css('MozUserSelect', 'none');
-            } else if ($.browser.msie) {//IE
-                $(this).bind('selectstart', function () { return false; });
-            } else {//Opera, etc.
-                $(this).mousedown(function () { return false; });
-            }
+            $(this).bind('selectstart click mousedown', function () { return false; });
         });
     });
 });
